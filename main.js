1140d50c05bad25b3a3705d87925ae3050cd2c65
Nithach Vassanapong <Bom-_2321@outlook.co.th>
2021-02-03 10:38:38 +0700

    [new] study syntax vue3

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const app = {
     data() {
         return {
-            msg: 'Hello, Vue3'
+            msg: 'Hello, Vue3',
+            shown: true,
+            url: 'http://sit.kmutt.ac.th'
         }
+    },
+    created() {
+        console.log('message is ' + this.msg)
+    },
+    updated(){
+        console.log('message is to ' + this.msg)
     }
-
 }
-Vue.createApp(app).mount('#app')
+var mountApp = Vue.createApp(app).mount('#app')
